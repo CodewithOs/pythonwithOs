88a04a3c4c9d2586e8e4471973cdce01d61f2a06
CodewithOs <68074329+CodewithOs@users.noreply.github.com>
2020-08-20 10:52:23 +0100

    Add files via upload

---
+++ LightBulb.py
@@ -0,0 +1,18 @@
+ls1 = False
+ls2 = True
+ls3 = False
+lightBulb1 = False
+lightBulb2 = False
+
+if ls1:
+    lightBulb1 = True
+
+if ls2 and ls3:
+    lightBulb2 = False
+
+
+
+if lightBulb1 or lightBulb2:
+    lightBulb3 = True
+    print("light bulb is on")
+    
